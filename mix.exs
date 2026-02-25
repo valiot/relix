de121d0f54b82a68a5085508b4c94abc762fa575
Ricardo Corral <ricardo.corral@valiot.io>
2026-02-25 12:28:45 -0600

    add hex.pm package requirements

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Relix.MixProject do
       version: "0.1.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Remote Execution Library for Elixir",
+      package: package(),
+      source_url: "https://github.com/valiot/relix"
     ]
   end
 
@@ -19,11 +22,19 @@ defmodule Relix.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Valiot"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/valiot/relix"},
+      files: ~w(lib/relix lib/relix.ex priv .formatter.exs mix.exs README.md LICENSE CHANGELOG.md)
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
